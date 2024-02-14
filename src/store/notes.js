e93d0e99ebe24bdac238fa39fcfd66fd95fb0c1e
kostastepetes <st1064973@ceid.upatras.gr>
2024-02-14 16:26:40 +0200

    NOTE Pinia LocalStorage

--- src/store/notes.js
+++ src/store/notes.js
@@ -1,18 +1,21 @@
-import { defineStore } from 'pinia'
+import { defineStore } from 'pinia';
 
 export const useNoteStore = defineStore('note', {
   state: () => ({
-    notes: [],
+    notes: JSON.parse(localStorage.getItem('notes')) || [],
   }),
   actions: {
     addNote(newNote) {
-      this.notes.push(newNote)
-      this.$patch()
+      this.notes.push(newNote);
+      this.saveNotesToLocalStorage();
     },
     removeNote(index) {
-      this.notes.splice(index,  1)
-      this.$patch()
+      this.notes.splice(index,  1);
+      this.saveNotesToLocalStorage();
+    },
+    saveNotesToLocalStorage() {
+      localStorage.setItem('notes', JSON.stringify(this.notes));
     },
     // Other actions for managing notes
   },
-})
+});
